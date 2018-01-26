63a62afbd360049df6375b28741bdd3543c1dcd5
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-01-26 15:47:14 -0200

    Adicionar listagem de repositórios

--- src/js/user.js
+++ src/js/user.js
@@ -1,7 +1,23 @@
+const handleReposResponses = ([reposTable, reposData]) => {
+  const template = Handlebars.compile(reposTable)({
+    repos: reposData
+  });
+  $("#view").append(template);
+  $("#reposTable").DataTable({
+    order: [[1, "desc"]]
+  });
+};
+
 const handleCardResponses = ([card, userDetail]) => {
   const template = Handlebars.compile(card)(userDetail);
   $("#view").empty();
   $("#view").append(template);
+
+  const repos = userDetail.repos_url;
+  Promise.all([
+    fetch("tpl/userReposList.html").then(resp => resp.text()),
+    fetch(repos).then(resp => resp.json())
+  ]).then(handleReposResponses);
 };
 
 const UserHandler = user => {
