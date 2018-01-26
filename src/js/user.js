c6332a2b5fb45fa6e0f0a27b983132ac27c67346
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-01-26 16:05:33 -0200

    Adicionar onclick para ir para página do repo

--- src/js/user.js
+++ src/js/user.js
@@ -2,10 +2,21 @@ const handleReposResponses = ([reposTable, reposData]) => {
   const template = Handlebars.compile(reposTable)({
     repos: reposData
   });
+
   $("#view").append(template);
   $("#reposTable").DataTable({
     order: [[1, "desc"]]
   });
+  document.querySelector("#reposTable tbody").addEventListener(
+    "click",
+    e => {
+      e.preventDefault();
+
+      const { fullname, user } = e.srcElement.parentElement.dataset;
+      location.hash = `#/user/${fullname}`;
+    },
+    false
+  );
 };
 
 const handleCardResponses = ([card, userDetail]) => {
