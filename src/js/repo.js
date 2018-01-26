dfcb5d68cf3c13b3ffdf404c8ab3d09d5608a867
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-01-26 17:03:44 -0200

    Fazer tratamente básico de erros

--- src/js/repo.js
+++ src/js/repo.js
@@ -1,4 +1,4 @@
-const handleResponses = ([html, repoData]) => {
+const handleRepoResponses = ([html, repoData]) => {
   const template = Handlebars.compile(html)(repoData);
   $("#view").empty();
   $("#view").append(template);
@@ -7,7 +7,11 @@ const handleResponses = ([html, repoData]) => {
 const RepoHandler = (user, repo) => {
   const fullName = `${user}/${repo}`;
   Promise.all([
-    fetch("tpl/repo.html").then(resp => resp.text()),
-    fetch(`https://api.github.com/repos/${fullName}`).then(resp => resp.json())
-  ]).then(handleResponses);
+    myFetch("tpl/repo.html").then(resp => resp.text()),
+    myFetch(`https://api.github.com/repos/${fullName}`).then(resp =>
+      resp.json()
+    )
+  ])
+    .then(handleRepoResponses)
+    .catch(handleErrors);
 };
--- src/js/user.js
+++ src/js/user.js
@@ -19,21 +19,27 @@ const handleReposResponses = ([reposTable, reposData]) => {
   );
 };
 
-const handleCardResponses = ([card, userDetail]) => {
+const handleUserResponses = ([card, userDetail]) => {
   const template = Handlebars.compile(card)(userDetail);
   $("#view").empty();
   $("#view").append(template);
 
   const repos = userDetail.repos_url;
   Promise.all([
-    fetch("tpl/userReposList.html").then(resp => resp.text()),
-    fetch(repos).then(resp => resp.json())
-  ]).then(handleReposResponses);
+    myFetch("tpl/userReposList.html").then(resp => resp.text()),
+    myFetch(repos).then(resp => resp.json())
+  ])
+    .then(handleReposResponses)
+    .catch(handleErrors);
 };
 
 const UserHandler = user => {
   Promise.all([
-    fetch("tpl/userCard.html").then(resp => resp.text()),
-    fetch(`https://api.github.com/users/${user}`).then(resp => resp.json())
-  ]).then(handleCardResponses);
+    myFetch("tpl/userCard.html").then(resp => resp && resp.text()),
+    myFetch(`https://api.github.com/users/${user}`).then(
+      resp => resp && resp.json()
+    )
+  ])
+    .then(handleUserResponses)
+    .catch(handleErrors);
 };
