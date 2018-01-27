36d154442087cdeb2c3e9909f825d2f7e840bf87
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-01-27 12:46:29 -0200

    Iniciar a troca do Gulp para Webpack

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: ["./src/js/index.js", "./src/_scss/main.scss"],
+  output: {
+    filename: "js/index.js",
+    path: path.join(__dirname, "./build/")
+  },
+  devServer: {
+    contentBase: "./build"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader", options: { minimize: true } }]
+      },
+      {
+        test: /\.hbs$/,
+        loader: "handlebars-loader"
+      },
+      {
+        test: /\.(otf|eot|ttf|woff|svg|png|jpe?g)/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "./img/[name].[ext]",
+              limit: 10000
+            }
+          },
+          {
+            loader: "img-loader"
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: { minimize: true }
+            },
+            { loader: "postcss-loader" },
+            { loader: "sass-loader" }
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "stage-2"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new ExtractTextPlugin({
+      filename: "css/main.css"
+    })
+  ]
+};
